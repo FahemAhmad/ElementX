e6ec6591ba5ffe59625bf15bfaa72137f3769063
FahemAhmad <faheemahmad0108@gmail.com>
2023-06-10 15:12:16 +0500

    Project corrupted

--- src/components/Button/Button.styles.tsx
+++ src/components/Button/Button.styles.tsx
@@ -1,22 +1,64 @@
-// src/components/Button/Button.styles.ts
 import styled from "styled-components";
+import { defaultTheme, typeScale } from "../../utils";
 
-export const ButtonWrapper = styled.button`
-  padding: 0.5em 1em;
-  font-size: 1em;
-  border-radius: 4px;
-  border: none;
-  color: white;
-  background-color: ${(props) => props.theme.primaryColor};
+const Button = styled.button`
+  padding: 8px 12px;
+  font-size: ${typeScale.paragraph};
+  border-radius: 2px;
+  min-width: 100px;
   cursor: pointer;
-  transition: background-color 0.3s ease;
+  font-family: ${defaultTheme.primaryFont};
+  transition: background-color 0.2s linear, color 0.2s linear,
+    border 0.2s linear;
 
   &:hover {
-    background-color: ${(props) => props.theme.primaryColorDark};
+    background-color: ${defaultTheme.primaryHoverColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryHoverColor};
+    outline-offset: 2px;
+  }
+
+  &:active {
+    background-color: ${defaultTheme.primaryActiveColor};
+    border-color: ${defaultTheme.primaryActiveColor};
+    color: ${defaultTheme.textColorOnPrimary};
   }
 
   &:disabled {
-    background-color: ${(props) => props.theme.disabledColor};
     cursor: not-allowed;
+    background: none;
+    color: ${defaultTheme.disabled};
+    border: none;
   }
 `;
+
+const PrimaryButton = styled(Button)`
+  background-color: ${defaultTheme.primaryColor};
+  color: ${defaultTheme.textColorOnPrimary};
+  border: 2px solid transparent;
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    border-color: ${defaultTheme.disabled};
+  }
+`;
+
+const SecondaryButton = styled(Button)`
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    border: 2px solid ${defaultTheme.disabled};
+  }
+`;
+
+const TertiaryButton = styled(Button)`
+  border: 2px solid transparent;
+  color: ${defaultTheme.primaryColor};
+`;
+
+export { PrimaryButton, SecondaryButton, TertiaryButton };
